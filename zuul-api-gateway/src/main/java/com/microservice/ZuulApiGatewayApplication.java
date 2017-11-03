c71669dc3c64870d00107beb5c068015837f2e60
Chetan <chetanpatel2510@gmail.com>
2017-11-02 23:14:17 -0400

    Added Zuul proxy

--- zuul-api-gateway/src/main/java/com/microservice/ZuulApiGatewayApplication.java
+++ zuul-api-gateway/src/main/java/com/microservice/ZuulApiGatewayApplication.java
@@ -2,7 +2,9 @@ package com.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class ZuulApiGatewayApplication {
 
