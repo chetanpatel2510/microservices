0b3aa3620be0e4fa88a313470d7f8cce3de24b9b
Chetan <chetanpatel2510@gmail.com>
2017-11-03 07:21:53 -0400

    Updated properties

--- product-service/src/main/java/com/microservice/product/controller/ProductController.java
+++ product-service/src/main/java/com/microservice/product/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.microservice.product.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,14 +18,18 @@ import com.microservice.product.domain.Product;
  *
  */
 @RestController
+@RefreshScope
 public class ProductController {
+
+	@Value("${product.name}")
+	private String name;
 
 	@RequestMapping(value = "/getProductById/{id}", method = RequestMethod.GET)
 	public Product getProductById(@PathVariable Long id) {
 		System.out.println("Calling get Product By Id");
 		Product product = new Product();
 		product.setId(id);
-		product.setName("Sample Product Name");
+		product.setName(name);
 		product.setDescription("Product description");
 		product.setPrice(BigDecimal.TEN);
 		product.setSku("123");
