9d3bfe9ce428da8ade943ccff3dce3b2ad421e69
Chetan <chetanpatel2510@gmail.com>
2017-11-03 00:24:34 -0400

    Added comments

--- order-service/src/main/java/com/microservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/microservice/OrderServiceApplication.java
@@ -2,7 +2,16 @@ package com.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * This class will serve as order service. It will register itself as Eureka
+ * client to the eureka server.
+ * 
+ * @author Chetan
+ *
+ */
+@EnableEurekaClient
 @SpringBootApplication
 public class OrderServiceApplication {
 
