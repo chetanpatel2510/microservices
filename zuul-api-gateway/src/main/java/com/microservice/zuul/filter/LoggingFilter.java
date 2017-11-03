3b660db70f25d9f0c83fbeb2a10abd360aa7cded
Chetan <chetanpatel2510@gmail.com>
2017-11-03 00:21:59 -0400

    Updated logging filter

--- zuul-api-gateway/src/main/java/com/microservice/zuul/filter/LoggingFilter.java
+++ zuul-api-gateway/src/main/java/com/microservice/zuul/filter/LoggingFilter.java
@@ -3,10 +3,30 @@ package com.microservice.zuul.filter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Component;
+
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 
+@Component
 public class LoggingFilter extends ZuulFilter {
+
+	/**
+	 * 0 is the first order.
+	 */
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+
+	/**
+	 * pre means before calling internal service. post means after calling
+	 * internal service.
+	 */
+	@Override
+	public String filterType() {
+		return "pre";
+	}
 
 	@Override
 	public Object run() {
@@ -25,23 +45,6 @@ public class LoggingFilter extends ZuulFilter {
 	@Override
 	public boolean shouldFilter() {
 		return true;
-	}
-
-	/**
-	 * 0 is the first order.
-	 */
-	@Override
-	public int filterOrder() {
-		return 0;
-	}
-
-	/**
-	 * pre means before calling internal service. post means after calling
-	 * internal service.
-	 */
-	@Override
-	public String filterType() {
-		return "pre";
 	}
 
 }
