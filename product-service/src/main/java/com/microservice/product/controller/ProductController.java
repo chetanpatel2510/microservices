4ce3fb63fb3e04820e2cc1602a70d284a0ec0b7f
Chetan <chetanpatel2510@gmail.com>
2017-11-30 06:47:02 -0500

    Added products

--- product-service/src/main/java/com/microservice/product/controller/ProductController.java
+++ product-service/src/main/java/com/microservice/product/controller/ProductController.java
@@ -2,7 +2,6 @@ package com.microservice.product.controller;
 
 import java.math.BigDecimal;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,18 +20,15 @@ import com.microservice.product.domain.Product;
 @RefreshScope
 public class ProductController {
 
-	@Value("${product.name}")
-	private String name;
-
 	@RequestMapping(value = "/getProductById/{id}", method = RequestMethod.GET)
 	public Product getProductById(@PathVariable Long id) {
 		System.out.println("Calling get Product By Id");
 		Product product = new Product();
 		product.setId(id);
-		product.setName(name);
+		product.setName("");
 		product.setDescription("Product description");
 		product.setPrice(BigDecimal.TEN);
-		product.setSku("123");
+		product.setCode("123");
 		return product;
 
 	}
--- product-service/src/main/java/com/microservice/product/domain/Product.java
+++ product-service/src/main/java/com/microservice/product/domain/Product.java
@@ -2,103 +2,107 @@ package com.microservice.product.domain;
 
 import java.math.BigDecimal;
 
+//@Entity
+//@Table(name = "PRODUCT")
 public class Product {
 
 	/**
 	 * id declaration.
 	 */
+	// @Id
+	// @GeneratedValue(strategy = GenerationType.AUTO)
+	// @Column(name = "PRODUCT_ID")
 	private Long id;
 
 	/**
 	 * name declaration.
 	 */
+	// @Column(name = "PROD_NAME")
 	private String name;
 
 	/**
 	 * description declaration.
 	 */
+	// @Column(name = "PROD_DESC")
 	private String description;
 
 	/**
 	 * sku declaration.
 	 */
-	private String sku;
+	// @Column(name = "PROD_CODE")
+	private String code;
 
 	/**
 	 * price declaration.
 	 */
+	// @Column(name = "PROD_PRICE")
 	private BigDecimal price;
 
 	/**
-	 * @return the id
+	 * //@return the code
 	 */
-	public Long getId() {
-		return id;
+	public String getCode() {
+		return code;
 	}
 
 	/**
-	 * @param id
-	 *            the id to set
-	 */
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
-
-	/**
-	 * @param name
-	 *            the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	/**
-	 * @return the description
+	 * //@return the description
 	 */
 	public String getDescription() {
 		return description;
 	}
 
 	/**
-	 * @param description
-	 *            the description to set
+	 * //@return the id
 	 */
-	public void setDescription(String description) {
-		this.description = description;
+	public Long getId() {
+		return id;
 	}
 
 	/**
-	 * @return the sku
+	 * //@return the name
 	 */
-	public String getSku() {
-		return sku;
+	public String getName() {
+		return name;
 	}
 
 	/**
-	 * @param sku
-	 *            the sku to set
-	 */
-	public void setSku(String sku) {
-		this.sku = sku;
-	}
-
-	/**
-	 * @return the price
+	 * //@return the price
 	 */
 	public BigDecimal getPrice() {
 		return price;
 	}
 
 	/**
-	 * @param price
-	 *            the price to set
+	 * //@param code the code to set
+	 */
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	/**
+	 * //@param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * //@param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * //@param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * //@param price the price to set
 	 */
 	public void setPrice(BigDecimal price) {
 		this.price = price;
