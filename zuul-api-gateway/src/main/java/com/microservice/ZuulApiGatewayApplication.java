2e488bb0573dcc6de50a784590b7eaf8dc061c0b
Chetan <chetanpatel2510@gmail.com>
2017-12-05 06:28:14 -0500

    Fixed config override issue.

--- zuul-api-gateway/src/main/java/com/microservice/ZuulApiGatewayApplication.java
+++ zuul-api-gateway/src/main/java/com/microservice/ZuulApiGatewayApplication.java
@@ -2,10 +2,12 @@ package com.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @SpringBootApplication
+@EnableEurekaClient
 public class ZuulApiGatewayApplication {
 
 	public static void main(String[] args) {
